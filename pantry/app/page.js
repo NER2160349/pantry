48314363da46351019b8c0e95d4e418538a97c4c
Nereida <89960249+NER2160349@users.noreply.github.com>
2024-08-05 22:22:37 +0000

    completed inventory page

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,15 +1,16 @@
-import Image from "next/image";
-import {useState, useEffect} from 'react'
-import {firestore} from '@/firebase'
-import {Box, Modal, ModalRoot, Typeography} from '@mui/material'
-import {collection, deleteDoc, doc, getDocs, query, setDoc } from 'firebase/firestore'
+'use client'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import { firestore } from '@/firebase'
+import { Box, Modal, Typography, Stack, TextField, Button } from '@mui/material'
+import { collection, deleteDoc, doc, getDocs, query, setDoc, getDoc } from 'firebase/firestore'
 
 export default function Home() {
-  const {invertory, setsInvertory} = usedDates([])
-  const [open, setOpen] = useState ([])
-  const [itemName, setItemName] = userState([''])
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
 
-const  updateInventory = async () => {
+  const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'))
     const docs = await getDocs(snapshot)
     const inventoryList = []
@@ -26,12 +27,11 @@ const  updateInventory = async () => {
     const docRef = doc(collection(firestore, 'inventory'), item)
     const docSnap = await getDoc(docRef)
 
-    if(docSnap.exists()) {
-      const {quantity} = docSnap.data()
-      await setDoc(docRef, {quantity: quantity + 1})
-      }
-    else{
-      await setDoc(docRef, (quanity: 1))
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1 })
+    } else {
+      await setDoc(docRef, { quantity: 1 })
     }
 
     await updateInventory()
@@ -41,17 +41,16 @@ const  updateInventory = async () => {
     const docRef = doc(collection(firestore, 'inventory'), item)
     const docSnap = await getDoc(docRef)
 
-    if(docSnap.exists()) {
-      const {quantity} = docSnap.data()
-      if(quanity == 1) {
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      if (quantity === 1) {
         await deleteDoc(docRef)
       } else {
-        await setDoc(docRef, {quantity: quantity - 1})
+        await setDoc(docRef, { quantity: quantity - 1 })
       }
     }
 
     await updateInventory()
-
   }
 
   useEffect(() => {
@@ -61,18 +60,89 @@ const  updateInventory = async () => {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
-  return 
-  <Box
-    width="100vw"
-    height="100vh"
-    display="flex"
-    justifyContents="center"
-    alighItems="center"
-    gap=(2)
-
-    <Modal
-    open=(open)
-    onClose=(handleClose)></Modal>
-    <Typeography variant="hi">Inventory Management</Typeography>
-  </Box>
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+    >
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value)
+              }}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button variant="contained" onClick={handleOpen}>
+        Add New Item
+      </Button>
+      <Box border="1px solid #333" width="800px" bgcolor="#ADD8E6" display="flex" alignItems="center" justifyContent="center" mt={2} p={2}>
+        <Typography variant="h2" color="#333">
+          Inventory Items
+        </Typography>
+      </Box>
+      <Stack width="800px" height="300px" spacing={2} overflow="auto" mt={2} p={2}>
+        {inventory.map(({ name, quantity }) => (
+          <Box
+            key={name}
+            width="100%"
+            minHeight="150px"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            bgcolor="#f0f0f0"
+            padding={3}
+            mb={2}
+          >
+            <Typography variant="h5" color="#333" textAlign="center">
+              {name.charAt(0).toUpperCase() + name.slice(1)}
+            </Typography>
+            <Typography variant="h5" color="#333" textAlign="center">
+              {quantity}
+            </Typography>
+            <Button variant="contained" onClick={() => removeItem(name)}>
+              Remove
+            </Button>
+          </Box>
+        ))}
+      </Stack>
+    </Box>
+  )
 }
